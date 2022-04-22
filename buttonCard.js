ebaf7551b93a1d84946715676229f4ee1cc771a0
azumydori <vlaplana1090@gmail.com>
2022-04-22 17:17:53 +0200

    PlexusJs

---
+++ buttonCard.js
@@ -0,0 +1,137 @@
+//LONDON
+
+function changeLondon() {
+
+    var divchange = document.getElementById('cardLondon');
+    if (divchange.style.height == '') {
+        divchange.style.height = '32rem'
+    } else {
+        divchange.style.height = ''
+    }
+
+    var buttonchange = document.getElementById('buttonLondon');
+    if (buttonchange.style.transform == '') {
+        buttonchange.style.transform = 'rotate(180deg)'
+        buttonchange.style.height 
+    } else {
+        buttonchange.style.transform = ''
+    }
+
+    var buttonheight = document.getElementById('buttonLondon');
+    if (buttonheight.style.marginTop == '') {
+        buttonheight.style.marginTop = '8rem'
+    } else {
+        buttonheight.style.marginTop = ''
+    }
+
+    var textappear = document.getElementById('textLondon');
+    if (textappear.style.overflow == '') {
+        textappear.style.overflow = 'visible'
+    } else {   
+        textappear.style.overflow = ''
+    }
+
+};
+
+//NEW YORK
+
+function changeNewYork() {
+
+    var divchange = document.getElementById('cardNewYork');
+    if (divchange.style.height == '') {
+        divchange.style.height = '32rem'
+    } else {
+        divchange.style.height = ''
+    }
+
+    var buttonchange = document.getElementById('buttonNewYork');
+    if (buttonchange.style.transform == '') {
+        buttonchange.style.transform = 'rotate(180deg)'
+        buttonchange.style.height 
+    } else {
+        buttonchange.style.transform = ''
+    }
+
+    var buttonheight = document.getElementById('buttonNewYork');
+    if (buttonheight.style.marginTop == '') {
+        buttonheight.style.marginTop = '8rem'
+    } else {
+        buttonheight.style.marginTop = ''
+    }
+
+    var textappear = document.getElementById('textNewYork');
+    if (textappear.style.overflow == '') {
+        textappear.style.overflow = 'visible'
+    } else {   
+        textappear.style.overflow = ''
+    }
+};
+
+
+//HONG KONG
+
+function changeHongKong() {
+
+    var divchange = document.getElementById('cardHongKong');
+    if (divchange.style.height == '') {
+        divchange.style.height = '32rem'
+    } else {
+        divchange.style.height = ''
+    }
+
+    var buttonchange = document.getElementById('buttonHongKong');
+    if (buttonchange.style.transform == '') {
+        buttonchange.style.transform = 'rotate(180deg)'
+        buttonchange.style.height 
+    } else {
+        buttonchange.style.transform = ''
+    }
+
+    var buttonheight = document.getElementById('buttonHongKong');
+    if (buttonheight.style.marginTop == '') {
+        buttonheight.style.marginTop = '8rem'
+    } else {
+        buttonheight.style.marginTop = ''
+    }
+
+    var textappear = document.getElementById('textHongKong');
+    if (textappear.style.overflow == '') {
+        textappear.style.overflow = 'visible'
+    } else {   
+        textappear.style.overflow = ''
+    }
+};
+
+//SIDNEY
+
+function changeSidney() {
+
+    var divchange = document.getElementById('cardSidney');
+    if (divchange.style.height == '') {
+        divchange.style.height = '32rem'
+    } else {
+        divchange.style.height = ''
+    }
+
+    var buttonchange = document.getElementById('buttonSidney');
+    if (buttonchange.style.transform == '') {
+        buttonchange.style.transform = 'rotate(180deg)'
+    } else {
+        buttonchange.style.transform = ''
+    }
+
+    var buttonheight = document.getElementById('buttonSidney');
+    if (buttonheight.style.marginTop == '') {
+        buttonheight.style.marginTop = '8rem'
+    } else {
+        buttonheight.style.marginTop = ''
+    }
+    
+
+    var textappear = document.getElementById('textSidney');
+    if (textappear.style.overflow == '') {
+        textappear.style.overflow = 'visible'
+    } else {   
+        textappear.style.overflow = ''
+    }
+};
